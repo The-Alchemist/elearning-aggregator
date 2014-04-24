cff8cc1a843bb0fc9d60225354c1833a3bddaf68
The Alchemist <kap4020@gmail.com>
2014-04-23 21:00:50 -0400

    adding REST client, which is needed for OCW API

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [korma "0.3.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [mysql/mysql-connector-java "5.1.30"]
                  [clojurewerkz/propertied "1.2.0"]
-                 ])
+                 [clj-http "0.9.1"]
+                ])
