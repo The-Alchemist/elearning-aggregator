41355ebed6b6fdbe6efca8e2ba82879fd17ddd6b
The_Alchemist <kap4020@gmail.com>
2014-04-23 22:25:22 -0400

    order of functions was messed up

--- src/elearning_aggregator/common.clj
+++ src/elearning_aggregator/common.clj
@@ -1,13 +1,16 @@
 (ns elearning-aggregator.common)
 
 
-(defn clean-data [data type]
-  (def data1 (for [ u data ] (dissoc u :links)))
-  (def data2 (for [ u data1 ] (clojure.set/rename-keys u (get-mapping type))))
-  (def data3 (for [ u data2 ] (map-without-empty-values u)))
-  data3)
-
 (defn map-without-empty-values [m]
   (into {}
     ; note: v could be a string or int (e.g., "abc" or 123), so empty? by itself doesn't work
     (for [ [k v] m :when (not= v "") ] [ k v ] )))
+
+(defn clean-data [data type mapping-fn]
+  (def data1 (for [ u data ] (dissoc u :links)))
+  (def data2 (for [ u data1 ] (clojure.set/rename-keys u (mapping-fn type))))
+  (def data3 (for [ u data2 ] (map-without-empty-values u)))
+  data3)
+
+
+
