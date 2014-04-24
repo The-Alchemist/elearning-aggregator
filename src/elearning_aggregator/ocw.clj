b12068380493c69d1effacbf5f5256c09be546ff
The-Alchemist <kap4020@gmail.com>
2014-04-23 22:49:44 -0400

    methods were in the wrong order

--- src/elearning_aggregator/ocw.clj
+++ src/elearning_aggregator/ocw.clj
@@ -3,14 +3,15 @@
             [clojure.string :as str]
             [clj-http.client :as client]))
 
+(defn make-url
+  "Creates a URL by joining the fields with a comma and adding that to a URL"
+  [url type]
+	    (str url type))
+
 (def url "http://data.ocwconsortium.org/api/v1/")
 
 (defn get-data [type]
     (client/get (make-url url type) {:as :json}) )
 
-(defn make-url
-  "Creates a URL by joining the fields with a comma and adding that to a URL"
-  [url type]
-	    (str url type))
 
 
